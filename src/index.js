21805134995695a66805b05f309ab11a8bfbf52a
Abhishek <amkb222@gmail.com>
2024-02-24 17:16:33 +0530

    new commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+
 // const myelement = (
 //   <table>
 //     <tr>
@@ -49,10 +50,70 @@ import ReactDOM from 'react-dom/client';
 // }
 // const myelement = <h1>{text}</h1>;
 
-const x = 5;
-const myelement = <h1>{(x) < 10 ? "Hello" : "Goodbye"}</h1>;
+// const x = 5;
+// const myelement = <h1>{(x) < 10 ? "Hello" : "Goodbye"}</h1>;
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(myelement); 
+// function Car() {
+//     return <h2>I am a Car!</h2>;
+// } 
+
+// function Garage() {
+//     return (
+//         <>
+//             <h1>Who lives in my Garage?</h1>
+//             <Car />             
+//         </>
+//     );
+// }
+
+// function Car(props){
+//     return <h2>I am a {props.brand.model }!</h2>;
+// }
+// function Garage(){
+//     const carinfo = {name: "Ford", model: "Mustang"};
+//     return (
+//         <div>
+//             <h1>Who lives in my Garage?</h1>
+//             <Car brand={carinfo}/>
+//         </div>
+//     );
+// }
+
+// function Football(){
+//     const shoot = (a) => {
+//         alert(a);
+//     }
+//     return(
+//         <button onClick={()=> shoot("Goal!")}>Take the shot!</button>
+//     );
+// }
+
+// function Goal(props){
+//     const isGoal = props.isGoal;
+//     if(isGoal){
+//         return <h1>Goal!</h1>
+//     }
+//     return <h1>Not Goal!</h1>
+// }
+
+function Garage(props){
+    const cars = props.cars;
+    return(
+        <>
+        <h1>Garage</h1>
+        {cars.length > 0 && 
+        <h2>
+            You have {cars.length} cars in your garage.
+        </h2>
+        }
+        </>
+    );
+}
+const cars = ['Ford', 'BMW', 'Fiat'];
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(myelement); 
+root.render(<Garage cars = {cars}/>);
 
 
